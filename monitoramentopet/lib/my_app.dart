ebf5fb930aed0e94c2787575f3e70af58981c371
LucianoDDario <lucianodario463@gmail.com>
2025-10-07 00:19:03 -0300

    Criação de pastas e tela de carregamento com o visual feito

---
+++ monitoramentopet/lib/my_app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:monitoramentopet/pages/carregamento.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        textTheme: GoogleFonts.ibmPlexSansTextTheme()
+      ),
+      home: const TelaCarregamento()
+    );
+  }
+}
